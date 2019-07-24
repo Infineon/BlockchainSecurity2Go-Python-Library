af63be0797e4ed0fe2355fd1b31f4fdc4de64271
Dhruv Khemani <gps.135.gps@gmail.com>
2019-07-24 17:45:10 +0200

    Add feature to find card reader via name

--- blocksec2go/commands.py
+++ blocksec2go/commands.py
@@ -1,6 +1,38 @@
 import logging
 logger = logging.getLogger(__name__)
 
+from smartcard.System import readers
+from blocksec2go.comm.pyscard import open_pyscard
+
+def find_reader(reader_name):
+    """ Looks for a specific card reader
+
+    Tries to find a card reader with specified name.
+
+    Args:
+        reader_name (str): string providing name of reader
+
+    Returns:
+        reader:
+            :obj:`PyScardReader`: A card reader with the name 
+            "reader_name". Chooses first reader with that name 
+            if multiple found.
+
+    Raises:
+        ReaderError: No reader found with that name.
+        
+        Any exceptions thrown by the reader wrapper are passed through.
+    """
+    logger.debug('FIND READER name %s', reader_name)
+    r = readers()
+    for reader in r:
+        if reader_name in str(reader):
+            try:
+                return open_pyscard(r[r.index(reader)])
+            except:
+                raise Exception('No card on reader')
+    raise Exception('No reader found')
+
 def select_app(reader):
     """ Sends command to select the Blockchain Security2GO application
 
