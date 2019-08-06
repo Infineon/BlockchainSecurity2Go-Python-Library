de189ad616d54ec1399d102c090d615aa3723bda
Dhruv Khemani <gps.135.gps@gmail.com>
2019-08-06 16:01:38 +0200

    Fix output message

--- blocksec2go/cli/unlock_pin.py
+++ blocksec2go/cli/unlock_pin.py
@@ -9,12 +9,16 @@ from blocksec2go.util import bytes_from_hex
 def _unlock_pin(args):
     reader = open_pyscard(args.reader)
     select_app(reader)
-    unlock_pin(reader, args.puk)
-
-    if args.machine_readable:
-        json.dump({'status': 'success'}, fp=sys.stdout)
+    status = unlock_pin(reader, args.puk)
+    if(True == status):
+        if args.machine_readable:
+            json.dump({'status': 'success'}, fp=sys.stdout)
+        else:
+            print('OK - unlocked')
+    elif(0 != status):
+        print('ERROR - ' + str(status) + ' tries left')
     else:
-        print('OK - unlocked')
+        print('ERROR - Locked')
 
 def add_subcommand(subparsers):
     parser = subparsers.add_parser('unlock_pin', description='Unlock a locked card')
